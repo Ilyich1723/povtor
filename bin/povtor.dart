ff8eed2a6b85e8c7c4ed72d8604e86fb98a2e0cc
Никита <ilyinnikita90@mail.ru>
2023-10-05 20:37:29 +0300

    ds

--- bin/povtor.dart
+++ bin/povtor.dart
@@ -1,10 +1,16 @@
 import 'package:dio/dio.dart';
 
+import 'models/getProd/getProd.dart';
+
 void main(List<String> arguments) async {
   Dio httpClient = Dio();
   String url ='https://dummyjson.com/products';
   var response = await httpClient.get(url);
 
-  Map<String,dynamic> json = response.data;
+ GetProd getProd = GetProd.fromJson(response.data);
+ 
   
+  for (var el in getProd.p) {
+    print(el.title);
+  }
 }
